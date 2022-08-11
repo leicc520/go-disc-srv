3fcdfc653f2ee0596f897f9b5390f914507dbccc
leicc520 <leicc123@gmail.com>
2022-08-11 15:42:40 +0800

    bug fixed regsrv

--- app/sysmsrv.go
+++ app/sysmsrv.go
@@ -1,13 +1,14 @@
 package app
 
 import (
+	"github.com/leicc520/go-gin-http/micro"
 	"strings"
 	
-	"github.com/leicc520/go-gin-http"
-	"github.com/leicc520/go-orm"
+	"github.com/gin-gonic/gin"
 	"github.com/leicc520/go-disc-srv/app/models"
 	"github.com/leicc520/go-disc-srv/app/service"
-	"github.com/gin-gonic/gin"
+	"github.com/leicc520/go-gin-http"
+	"github.com/leicc520/go-orm"
 )
 
 type argsSysMsrvListSt struct {
@@ -62,11 +63,12 @@ func sysmsrvStatus(c *gin.Context) {
 	if err := sorm.GetOne(args.Id).ToStruct(&data); err != nil || data.Id < 1 {
 		core.PanicHttpError(1002, "请求的微服务不存在哟.")
 	}
+	regSrv := micro.NewRegSrvClient("")
 	if args.Status == 1 { //上线注册服务  更新数据让业务代码自己做
-		if !core.RegSrv.Health(1, data.Proto, data.Srv) {
+		if !regSrv.Health(1, data.Proto, data.Srv) {
 			core.PanicHttpError(1004, "该服务心跳检查异常,无法上架.")
 		}
-		core.RegSrv.Register(data.Name, data.Srv, data.Proto, data.Version)
+		regSrv.Register(data.Name, data.Srv, data.Proto, data.Version)
 	} else { //注销离线的处理逻辑
 		nSize := sorm.GetTotal(func(st *orm.QuerySt) string {
 			st.Where("name", data.Name).Where("proto", data.Proto)
@@ -76,7 +78,7 @@ func sysmsrvStatus(c *gin.Context) {
 		if nSize < 1 {//没有其他的可用服务的情况
 			core.PanicHttpError(1003, "无其他可用服务,不允许下架啦.")
 		}
-		core.RegSrv.UnRegister(data.Proto, data.Name, data.Srv)
+		regSrv.UnRegister(data.Proto, data.Name, data.Srv)
 	}
 	core.NewHttpView(c).JsonDisplay(nil)
 }
@@ -88,7 +90,8 @@ func sysmsrvStatus(c *gin.Context) {
 // @Success 200 {json} HttpView {sid:xxx[要记录,以后请求放http请求头中]}
 // @Router /api/core/misrv/reload [post]
 func sysmsrvReload(c *gin.Context) {
-	if err := core.RegSrv.Reload(); err != nil {
+	regSrv := micro.NewRegSrvClient("")
+	if err := regSrv.Reload(); err != nil {
 		core.PanicHttpError(500, err.Error())
 	}
 	core.NewHttpView(c).JsonDisplay(nil)
